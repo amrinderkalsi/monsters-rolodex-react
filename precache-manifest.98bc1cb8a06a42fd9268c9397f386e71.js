4e8c334d8a96839de52d6dbca30e6a1384469d50
Amrinder Kalsi <amrinder.kalsi@agnition.ca>
2019-07-12 13:34:53 -0400

    Updates

---
+++ precache-manifest.98bc1cb8a06a42fd9268c9397f386e71.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4209901ab9a751a9f09e2c72b03638c7",
+    "url": "/monsters-rolodex-react/index.html"
+  },
+  {
+    "revision": "ced1b21fd51e09a8bd70",
+    "url": "/monsters-rolodex-react/static/css/main.0bfdb9ed.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex-react/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "ced1b21fd51e09a8bd70",
+    "url": "/monsters-rolodex-react/static/js/main.508070b4.chunk.js"
+  },
+  {
+    "revision": "590cdc23d4720e222623",
+    "url": "/monsters-rolodex-react/static/js/runtime~main.56d562c4.js"
+  }
+]);
